c1ea1a40a7e1262b56d952e7796235eb21ebe0d6
gabrielmchlmnn <michelmanngabriel@gmail.com>
2023-07-06 16:08:41 -0300

    telas modificadas

--- conveniencia_tcc/settings.py
+++ conveniencia_tcc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,4 +153,10 @@ if not DEBUG:
     ]
 
 
-STATIC_URL = 'templates/static/'
+STATIC_URL = '/templates/static/'
+
+STATICFILES_DIR = [ 
+     os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
